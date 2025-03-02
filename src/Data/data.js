b91ca757a3e6f73d77fac42ed6cb3cd820eaabdb
hkaur108 <hkaur108@hotmail.com>
2025-03-01 19:36:08 -0500

    fixed links for projects

--- src/Data/data.js
+++ src/Data/data.js
@@ -3,7 +3,7 @@ export const Project=[
     id:1,
     name:"Todo list Project",
     img: process.env.PUBLIC_URL +'/Images/projectImages/todo.png',
-    gitLink:'https://hkaur108.github.io/todolist/'
+    gitLink:'https://hkaur108.github.io/Todo-App-with-React-Redux-Typescript/'
   },
     {  
     id:2,
@@ -16,7 +16,7 @@ export const Project=[
     id:17,
     name:"Mortgage Calculator",
     img:process.env.PUBLIC_URL +"/Images/projectImages/mortgage.png",
-    gitLink:'https://hkaur108.github.io/mortgage/'
+    gitLink:'https://hkaur108.github.io/Mortgage-Calculator-with-Redux-Typescript/'
 },
 {  
     id:18,
@@ -31,12 +31,12 @@ export const Project=[
     img:process.env.PUBLIC_URL +'/Images/projectImages/shopp.png',
     gitLink:'https://hkaur108.github.io/ShoppingCart/'
 },
- {  
-    id:4,
-    name:"Tic Tac Toe Game",
-    img:process.env.PUBLIC_URL +'/Images/projectImages/tictac.jpeg',
-    gitLink:'https://hkaur108.github.io/tictactoe/'
-},
+//  {  
+//     id:4,
+//     name:"Tic Tac Toe Game",
+//     img:process.env.PUBLIC_URL +'/Images/projectImages/tictac.jpeg',
+//     gitLink:'https://hkaur108.github.io/tictactoe/'
+// },
 //  {  
 //     id:5,
 //     name:"URL Shortner Project",
@@ -50,25 +50,30 @@ export const Project=[
 //     gitLink:'https://github.com/hkaur108/shopping-cart.git'
 // }
 
-
  {  
-    id:5,
-    name:"Rock Paper Scissor",
+    id:19,
+    name:"Product Cart",
     img:process.env.PUBLIC_URL +"/Images/projectImages/rock.png",
-    gitLink:'https://hkaur108.github.io/rockPaperScissors/'
+    gitLink:'https://hkaur108.github.io/Todo-App-with-React-Redux-Typescript/'
 },
- {  
-    id:6,
-    name:"Pomodoro Timer",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/timer",
-    gitLink:'https://hkaur108.github.io/Pomodoro/'
-},
- {  
-    id:7,
-    name:"Quotes Generator",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/quote",
-    gitLink:'https://hkaur108.github.io/randomquote/'
-},
+//  {  
+//     id:5,
+//     name:"Rock Paper Scissor",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/rock.png",
+//     gitLink:'https://hkaur108.github.io/rockPaperScissors/'
+// },
+//  {  
+//     id:6,
+//     name:"Pomodoro Timer",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/timer",
+//     gitLink:'https://hkaur108.github.io/Pomodoro/'
+// },
+//  {  
+//     id:7,
+//     name:"Quotes Generator",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/quote",
+//     gitLink:'https://hkaur108.github.io/randomquote/'
+// },
  {  
     id:8,
     name:"Google Keeper",
@@ -76,51 +81,51 @@ export const Project=[
     gitLink:'https://hkaur108.github.io/keeper/'
 },
 
-{  
-    id:9,
-    name:"Flash Cards",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/flash-cards.jpg",
-    gitLink:'https://hkaur108.github.io/flashy/'
-},
+// {  
+//     id:9,
+//     name:"Flash Cards",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/flash-cards.jpg",
+//     gitLink:'https://hkaur108.github.io/flashy/'
+// },
 
-{  
-    id:10,
-    name:"Quiz",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/quiz",
-    gitLink:'https://hkaur108.github.io/quiz/'
-},
+// {  
+//     id:10,
+//     name:"Quiz",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/quiz",
+//     gitLink:'https://hkaur108.github.io/quiz/'
+// },
 
-{  
-    id:11,
-    name:"Add Book",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/addBook.png",
-    gitLink:'https://hkaur108.github.io/addBook/'
-},
-{  
-    id:13,
-    name:"Dice Game",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/d1.png",
-    gitLink:'https://hkaur108.github.io/rolldice/'
-},
-{  
-    id:14,
-    name:"DrumKit",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/drum.png",
-    gitLink:'https://hkaur108.github.io/drumkit/'
-},
+// {  
+//     id:11,
+//     name:"Add Book",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/addBook.png",
+//     gitLink:'https://hkaur108.github.io/addBook/'
+// },
+// {  
+//     id:13,
+//     name:"Dice Game",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/d1.png",
+//     gitLink:'https://hkaur108.github.io/rolldice/'
+// },
+// {  
+//     id:14,
+//     name:"DrumKit",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/drum.png",
+//     gitLink:'https://hkaur108.github.io/drumkit/'
+// },
 
-{  
-    id:15,
-    name:"Send Message",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/message.png",
-    gitLink:'https://hkaur108.github.io/passmessage/'
-},
-{  
-    id:16,
-    name:"Simon Game",
-    img:process.env.PUBLIC_URL +"/Images/projectImages/simon.png",
-    gitLink:'https://hkaur108.github.io/simon/'
-},
+// {  
+//     id:15,
+//     name:"Send Message",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/message.png",
+//     gitLink:'https://hkaur108.github.io/passmessage/'
+// },
+// {  
+//     id:16,
+//     name:"Simon Game",
+//     img:process.env.PUBLIC_URL +"/Images/projectImages/simon.png",
+//     gitLink:'https://hkaur108.github.io/simon/'
+// },
 
 
 
